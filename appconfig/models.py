cbaccce4dd8b2d41c3c117bf6299ef2cd6e1dfa7
kuttamuwa <ucok.umut@gmail.com>
2020-11-21 20:44:53 +0300

    appconfig added

---
+++ appconfig/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from risk_analysis.models import BaseModel
+
+
+# Create your models here.
+# Puantage #
+class Domains(BaseModel):
+    name = models.CharField(db_column='DOMAIN', max_length=100)
+    point = models.FloatField(max_length=100, default=0.0, db_column='POINT',
+                              help_text='Set your domain point of your variable'  # , validators=[validate_summary]
+                              )
+
+    @staticmethod
+    def get_total_points():
+        return sum(i.point for i in Domains.objects.all())
+
+    def save(self, *args, **kwargs):
+        if self.point + self.get_total_points() <= 100:
+            super(Domains, self).save(*args, **kwargs)
+
+        else:
+            raise ValueError("Total of points exceeds 100")
+
+    def __str__(self):
+        return f'Domain: {self.name} \n' \
+               f'General Point : {self.point}'
+
+    class Meta:
+        db_table = 'DOMAINS'
+
+
+class Subtypes(BaseModel):
+    domain_name = models.ForeignKey(Domains, on_delete=models.PROTECT, max_length=100)
+
+    pts = models.FloatField(max_length=100, db_column='PTS', help_text='Point of specified intervals '
+                                                                       'of your subtype related Domain')
+    min_interval = models.FloatField(max_length=100, db_column='MIN_INTERVAL', help_text='Minimum interval')
+
+    max_interval = models.FloatField(max_length=100, db_column='MAX_INTERVAL', help_text='Maximum interval')
+
+    def save(self, *args, **kwargs):
+        # total point control
+        if not self.pts + self.get_total_points(self.domain_name) <= 100:
+            raise ValueError(f"Point exceeds 100 for this domain : {self.domain_name}")
+
+        # interval control
+        # todo: bunu sonra cozecegim.
+
+        super(Subtypes, self).save()
+
+    @staticmethod
+    def get_total_points(domain):
+        return sum(i.pts for i in Subtypes.objects.filter(domain_name=domain))
+
+    def __str__(self):
+        return f"Points of {self.domain_name} : \n" \
+               f"Minimum interval: {self.min_interval} \n" \
+               f"Maximum interval: {self.max_interval}"
+
+    class Meta:
+        db_table = 'SUBTYPES'
---
+++ appconfig/views.py
@@ -0,0 +1,167 @@
+from bootstrap_modal_forms.generic import BSModalFormView, BSModalCreateView, BSModalUpdateView, BSModalReadView, \
+    BSModalDeleteView
+from django.http import JsonResponse
+from django.shortcuts import render
+
+# Create your views here.
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy
+from django.views import generic
+
+from appconfig.forms import DomainFilterForm, DomainModalForm, SubtypeFilterForm, SubtypeModalForm
+from appconfig.models import Domains, Subtypes
+
+
+# indexes, main pages
+def all_indexes(request):
+    return render(request, 'appconfig/index.html')
+
+
+class SubtypeIndex(generic.ListView):
+    model = Subtypes
+    context_object_name = 'subtypes'
+    template_name = 'appconfig/subtypedirs/subtype_index.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if 'type' in self.request.GET:
+            qs = qs.filter(domain_name=str(self.request.GET['domain_name']))
+        return qs
+
+
+class DomainIndex(generic.ListView):
+    model = Domains
+    context_object_name = 'domains'
+    template_name = 'appconfig/domaindirs/domain_index.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if 'domain_name' in self.request.GET:
+            qs = qs.filter(domain_name=str(self.request.GET['domain_name']))
+        return qs
+
+
+# Domains
+class DomainFilterView(BSModalFormView):
+    template_name = 'appconfig/domaindirs/filter_domain.html'
+    form_class = DomainFilterForm
+
+    def form_valid(self, form):
+        if 'clear' in self.request.POST:
+            self.filter = ''
+        else:
+            self.filter = '?type=' + form.cleaned_data['type']
+
+        response = super().form_valid(form)
+        return response
+
+    def get_success_url(self):
+        return reverse_lazy('index') + self.filter
+
+
+class DomainCreateView(BSModalCreateView):
+    template_name = 'appconfig/domaindirs/create_domain.html'
+    form_class = DomainModalForm
+    success_message = 'Success: Domain was created.'
+    success_url = reverse_lazy('domain-index')
+
+    def form_valid(self, form):
+        return super(DomainCreateView, self).form_valid(form)
+
+
+class DomainUpdateView(BSModalUpdateView):
+    model = Domains
+    template_name = 'appconfig/domaindirs/update_domain.html'
+    form_class = DomainModalForm
+    success_message = 'Success: Domain was updated.'
+    success_url = reverse_lazy('domain-index')
+
+    def form_valid(self, form):
+        return super(DomainUpdateView, self).form_valid(form)
+
+
+class DomainReadView(BSModalReadView):
+    model = Domains
+    template_name = 'appconfig/domaindirs/read_domain.html'
+
+
+class DomainDeleteView(BSModalDeleteView):
+    model = Domains
+    template_name = 'appconfig/domaindirs/delete_domain.html'
+    success_message = 'Success: Domain was deleted.'
+    success_url = reverse_lazy('domain-index')
+
+
+def domains_list(request):
+    data = dict()
+    if request.method == 'GET':
+        domains = Domains.objects.all()
+        data['table'] = render_to_string(
+            'appconfig/domaindirs/_domains_table.html',
+            {'domains': domains},
+            request=request
+        )
+        return JsonResponse(data)
+
+
+# Subtypes
+class SubtypeFilterView(BSModalFormView):
+    template_name = 'appconfig/subtypedirs/filter_subtype.html'
+    form_class = SubtypeFilterForm
+
+    def form_valid(self, form):
+        if 'clear' in self.request.POST:
+            self.filter = ''
+        else:
+            self.filter = '?type=' + form.cleaned_data['type']
+
+        response = super().form_valid(form)
+        return response
+
+    def get_success_url(self):
+        return reverse_lazy('index') + self.filter
+
+
+class SubtypeCreateView(BSModalCreateView):
+    template_name = 'appconfig/subtypedirs/create_subtype.html'
+    form_class = SubtypeModalForm
+    success_message = 'Success: Subtype was created.'
+    success_url = reverse_lazy('subtype-index')
+
+    def form_valid(self, form):
+        return super(SubtypeCreateView, self).form_valid(form)
+
+
+class SubtypeUpdateView(BSModalUpdateView):
+    model = Subtypes
+    template_name = 'appconfig/subtypedirs/update_subtype.html'
+    form_class = SubtypeModalForm
+    success_message = 'Success: Subtype was updated.'
+    success_url = reverse_lazy('subtype-index')
+
+    def form_valid(self, form):
+        return super(SubtypeUpdateView, self).form_valid(form)
+
+
+class SubtypeReadView(BSModalReadView):
+    model = Subtypes
+    template_name = 'appconfig/subtypedirs/read_subtype.html'
+
+
+class SubtypeDeleteView(BSModalDeleteView):
+    model = Subtypes
+    template_name = 'appconfig/subtypedirs/delete_subtype.html'
+    success_message = 'Success: Subtype was deleted.'
+    success_url = reverse_lazy('subtype-index')
+
+
+def subtypes_list(request):
+    data = dict()
+    if request.method == 'GET':
+        subtypes = Subtypes.objects.all()
+        data['table'] = render_to_string(
+            'appconfig/subtypedirs/_subtypes_table.html',
+            {'subtypes': subtypes},
+            request=request
+        )
+        return JsonResponse(data)
